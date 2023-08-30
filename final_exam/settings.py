25af21f7da9bfe89bb8f4b99aa35962a1200b7b3
unknown <halima.seemab@gmail.com>
2023-08-30 13:50:27 -0700

    updeated exam file

--- final_exam/settings.py
+++ final_exam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5phfr+2!xrf6fm$2j*&@5$79l2um3#$kdqpcj4%(*(=!7ek(7!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
